1610e8293be7d85ad8021da2ec830671b5bea2eb
ari23yan <ariyan137923@yahoo.com>
2023-01-29 08:58:11 +0330

    Add GetHolidaysInMonth Method And Fix Some Changes

--- CalenderWebApi/Dtos/CalendarHolidayDto.cs
+++ CalenderWebApi/Dtos/CalendarHolidayDto.cs
@@ -3,8 +3,9 @@
     public class CalendarHolidayDto
     {
         public int Day { get; set; }
-        //public int Month { get; set; }
-        //public int Year { get; set; }
+        public string DayOfTheWeek { get; set; }
+        public string Month { get; set; }
+        public int Year { get; set; }
         public bool IsHoliday { get; set; }
         //public string Type { get; set; }
     }
